e3cda832aca18529bb3f1e82553f75851e6c0116
Ahmet-Kutuk <ahmetcankutk@gmail.com>
2022-08-22 19:16:49 +0300

    generate function implemented

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
+import constant from "./constant";
 
+const createLetterPool = (
+  hasNumber: boolean,
+  hasSpecial: boolean,
+  hasUpperCharacter: boolean,
+  hasLowerCharacter: boolean
+) => {
+  const numberList = constant.number;
+  const specialList = constant.specialCharacter;
+  const upperCharacterList = constant.upperCharacter;
+  const lowerCharacterList = constant.lowerCharacter;
+  let letterPool: Array<String[]> = [];
+  hasNumber && letterPool.push(numberList);
+  hasSpecial && letterPool.push(specialList);
+  hasUpperCharacter && letterPool.push(upperCharacterList);
+  hasLowerCharacter && letterPool.push(lowerCharacterList);
+
+  return letterPool;
+};
+
+export const GenerateUnique = (
+  maxLength: number,
+  hasNumber?: boolean,
+  hasSpecial?: boolean,
+  hasUpperCharacter?: boolean,
+  hasLowerCharacter?: boolean
+) => {
+  let createdUnique = "";
+  const pool = createLetterPool(
+    hasNumber,
+    hasSpecial,
+    hasUpperCharacter,
+    hasLowerCharacter
+  );
+
+  if (pool.length > 0) {
+    for (let index = 0; index < maxLength; index++) {
+      const randomArrayListIndex: number =
+        Math.ceil(Math.random() * pool.length) - 1;
+      const characterSet: String[] = pool[randomArrayListIndex];
+      const randomArrayValueIndex =
+        Math.ceil(Math.random() * characterSet.length) - 1;
+      createdUnique += characterSet[randomArrayValueIndex] as string;
+    }
+  }
+
+  return createdUnique;
+};
