7cb42a3da415ecdc85900878911120fcc256eaff
Ahmet-Kutuk <ahmetcankutk@gmail.com>
2022-08-22 17:08:00 +0300

    :wrench: inital commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: "none",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    clean: true,
+  },
+  watch: true,
+};
